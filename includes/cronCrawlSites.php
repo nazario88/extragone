8aff78d55c0a70831b373cbc899bc56fba4795e4
nazario88 <nazario.addict@gmail.com>
2025-10-26 14:16:03 +0100

    Suppression de l'affichage des erreurs.

--- includes/cronCrawlSites.php
+++ includes/cronCrawlSites.php
@@ -3,9 +3,11 @@
 // Script de vérification des liens eXtragone
 // =======================================================
 
+/*
 ini_set('display_errors', 1);
 ini_set('display_startup_errors', 1);
 error_reporting(E_ALL);
+*/
 set_time_limit(0);
 ini_set('memory_limit', '512M');
 
