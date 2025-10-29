86bd2bc6486eb9ce2990d1b8abae2133d297c4e2
nazario88 <nazario.addict@gmail.com>
2025-10-29 21:01:56 +0100

    Correction URL tag image-seo

--- includes/header.php
+++ includes/header.php
@@ -3,6 +3,9 @@ if(!isset($base)) $base = "https://www.extrag.one"; //defaut)
 if(!isset($url_canon)) $url_canon = "https://www.extrag.one"; //defaut)
 if(!isset($recherche)) $recherche = "";
 if(!isset($image_seo)) $image_seo = "$base/assets/img/image-og.png";
+if(isset($image_seo) && !str_starts_with($image_seo, 'https')) {
+  $image_seo = 'https:// '.$image_seo;
+}
 ?>
 <!DOCTYPE html>
 <html lang="fr" class="dark">
@@ -19,7 +22,7 @@ if(!isset($image_seo)) $image_seo = "$base/assets/img/image-og.png";
     <meta property="og:site_name"   content="eXtragone">
     <meta property="og:title"       content="<?=$title?>">
     <meta property="og:description" content="<?=$description?>">
-    <meta property="og:image"       content="https://extrag.one/<?=$image_seo?>">
+    <meta property="og:image"       content="<?=$image_seo?>">
     <meta property="og:type"        content="website">
     <meta property="og:url"         content="<?=$url_canon?>">
 
